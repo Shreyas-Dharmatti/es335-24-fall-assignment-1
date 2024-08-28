e4e8a43f3880972622a2f8b936d1dbb1026d4a6d
Shreyas Dharmatti <dharmattieshreyas@iitgn.ac.in>
2024-08-28 16:47:55 +0530

    update

--- metrics.py
+++ metrics.py
@@ -1,46 +1,76 @@
 from typing import Union
 import pandas as pd
-
-
 def accuracy(y_hat: pd.Series, y: pd.Series) -> float:
-    """
-    Function to calculate the accuracy
-    """
+    assert y_hat.size == y.size, "y_hat and y must have the same size"
+    
+    correct_predictions = 0
+    total_predictions = y.size
 
-    """
-    The following assert checks if sizes of y_hat and y are equal.
-    Students are required to add appropriate assert checks at places to
-    ensure that the function does not fail in corner cases.
-    """
-    assert y_hat.size == y.size
-    # TODO: Write here
-    pass
+    for i in range(total_predictions):
+        if y_hat[i] == y[i]:
+            correct_predictions += 1
 
+    return correct_predictions / total_predictions
 
 def precision(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
-    """
-    Function to calculate the precision
-    """
-    pass
 
+    true_positives = 0
+    false_positives = 0
+
+    for i in range(y.size):
+        if y_hat[i] == cls:
+            if y[i] == cls:
+                true_positives += 1
+            else:
+                false_positives += 1
+
+    if true_positives + false_positives == 0:
+        return 0.0  # Avoid division by zero
+
+    return true_positives / (true_positives + false_positives)
 
 def recall(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
-    """
-    Function to calculate the recall
-    """
-    pass
+    
+    true_positives = 0
+    false_negatives = 0
 
+    for i in range(y.size):
+        if y[i] == cls:
+            if y_hat[i] == cls:
+                true_positives += 1
+            else:
+                false_negatives += 1
+
+    if true_positives + false_negatives == 0:
+        return 0.0  # Avoid division by zero
+
+    return true_positives / (true_positives + false_negatives)
+
+import math
 
 def rmse(y_hat: pd.Series, y: pd.Series) -> float:
-    """
-    Function to calculate the root-mean-squared-error(rmse)
-    """
+   
+    assert y_hat.size == y.size, "y_hat and y must have the same size"
 
-    pass
+    sum_squared_errors = 0.0
+    n = y.size
 
+    for i in range(n):
+        error = y_hat[i] - y[i]
+        sum_squared_errors += error ** 2
+
+    mean_squared_error = sum_squared_errors / n
+    return math.sqrt(mean_squared_error)
 
 def mae(y_hat: pd.Series, y: pd.Series) -> float:
-    """
-    Function to calculate the mean-absolute-error(mae)
-    """
-    pass
+  
+    assert y_hat.size == y.size, "y_hat and y must have the same size"
+
+    sum_absolute_errors = 0.0
+    n = y.size
+
+    for i in range(n):
+        error = abs(y_hat[i] - y[i])
+        sum_absolute_errors += error
+
+    return sum_absolute_errors / n
